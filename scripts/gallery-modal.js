9b697a2f6f84924f2a8704b8220ab7aac6df267e
Tim Brooks <tbrook117@gmail.com>
2023-07-11 21:51:10 -0500

    Added Gallery modal and updated contact info

---
+++ scripts/gallery-modal.js
@@ -0,0 +1,50 @@
+(function($) {
+    let modalDiv = buildModalFromScratch();
+    $('#gallery img').on('click', (event) => {
+        $(modalDiv).removeAttr('hidden');
+        $(modalDiv).find('.modal-header').first().html('<h2>' + event.target.alt + '</h2>');
+
+        let img = document.createElement('img');
+        img.src = event.target.src;
+        img.alt = event.target.alt;
+
+        $(modalDiv).find('.modal-body').first().html(img);
+    });
+
+    $('.modal-btn').keypress((event) => {
+        if (event.keyCode === 13) {
+            $(".modal-btn").click();
+        }
+    });
+
+    $('.modal-btn').on('click', () => {
+        $('#gallery-modal-div').attr('hidden', '');
+    });
+})(jQuery);
+
+function buildModalFromScratch() {
+    let modalDiv = document.createElement('div');
+    modalDiv.id = 'gallery-modal-div';
+    modalDiv.setAttribute('hidden', '');
+    document.body.appendChild(modalDiv);
+
+    let modalInnerWrapper = document.createElement('div');
+    modalInnerWrapper.id = 'modal-inner-wrapper';
+    modalDiv.appendChild(modalInnerWrapper);
+
+    let modalHeader = document.createElement('div');
+    modalHeader.classList.add('modal-header');
+    let modalBody= document.createElement('div');
+    modalBody.classList.add('modal-body');
+    let modalFooter= document.createElement('div');
+    modalFooter.classList.add('modal-footer');
+
+    modalDismissBtn = document.createElement('button');
+    modalDismissBtn.textContent = 'Close';
+    modalDismissBtn.classList.add('modal-btn', 'btn', 'float-right');
+
+    modalFooter.appendChild(modalDismissBtn);
+    modalInnerWrapper.append(modalHeader, modalBody, modalFooter);
+
+    return modalDiv;
+}
